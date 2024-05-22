1e5c2e0fa35aa55cae52a862324bd20334400e9e
Guilherme Rera <guilhermerera@gmail.com>
2024-05-22 16:37:04 -0300

    remove: utils folder and function as it was not working inside puppeteer instance

--- utils/kabum-get-product-url.js
+++
@@ -1,6 +0,0 @@
-export function KabumGetProductURL(imageUrl) {
-	const urlPieces = imageUrl.split("/");
-	const productId = urlPieces[urlPieces.length - 1];
-
-	return `kabum.com.br/produto/${productId}`;
-}
